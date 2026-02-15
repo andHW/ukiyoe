154dcbf90f1849e16bfc38bd929729dbce3aadb2
Andreas <and@reas.me>
2026-02-15 00:53:55 -0800

    refactor: replace BrowserRouter with HashRouter for routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import GamePage from "./components/game/GamePage";
 import BoardBuilder from "./components/builder/BoardBuilder";
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/play" element={<GamePage />} />
         <Route path="/builder" element={<BoardBuilder />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
